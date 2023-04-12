8a89516d20c8f533378718dc1a63b23278a1c15d
lakdav <ladav2018@gmail.com>
2023-04-12 23:48:45 +0300

    feat: add sortedLinkedList

--- index.js
+++ index.js
@@ -1,16 +1,14 @@
 import { Deque } from './deque/index.js';
-import { DoublyLinkedList, LinkedList, CircularLinkedList } from './list/index.js';
+import { DoublyLinkedList, LinkedList, CircularLinkedList, SortedLinkedList } from './list/index.js';
 import { Queue } from './queues/index.js';
 import { Stack, StackArray } from './stacks/index.js';
 import { baseConverter, createMatrix, decimalToBinary } from './utils/index.js';
 import palindromeChecker from './utils/palindromeChecker.js';
 
-const list = new CircularLinkedList();
-list.insert(1, 0);
-list.insert(2, 1);
-list.insert(10, 1);
-list.insert(10, 1);
-list.insert(10, 1);
-list.insert(200, 4);
-list.removeAt(0);
+const list = new SortedLinkedList();
+list.insert(1);
+list.insert(1);
+list.insert(10);
+list.insert(2);
+list.insert(3);
 console.log(list.toString());
--- list/index.js
+++ list/index.js
@@ -1,3 +1,4 @@
 export { default as LinkedList } from './linkedList/LinkedList.js';
 export { default as DoublyLinkedList } from './doublyLinkedList/DoublyLinkedList.js';
 export { default as CircularLinkedList } from './circularLinkedList/CircularLinkedList.js';
+export { default as SortedLinkedList } from './sortedLinkedList/SortedLinkedList.js';
